e4c9e389f7c8c4a5ed22f2eeb12ca37433ea2c89
Sharan Ram <sharanram1993@gmail.com>
2017-07-17 14:42:46 +0530

    parse null,boolean and number

--- json-parser.js
+++ json-parser.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const nullParser = input => {
+  return input.startsWith("null") ? [null, input.slice(4)] : null;
+};
+const booleanParser = input => {
+  return input.startsWith("true")
+    ? [true, input.slice(4)]
+    : input.startsWith("false") ? [false, input.slice(5)] : null;
+};
+
+const numberParser = input => {
+  let numberPattern = /^[+-]?\d*\.?(\d+[eE]?[+-]?)?\d+$/;
+  let res = input.match(numberPattern);
+  return res ? [parseFloat(res[0]), input.slice(res[0].length)] : null;
+};
