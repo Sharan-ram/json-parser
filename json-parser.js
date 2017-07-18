2d7af342b0e2926a97dbe09f6e7322b7fe1b6fde
Sharan Ram <sharanram1993@gmail.com>
2017-07-18 10:01:51 +0530

    parse string and space

--- json-parser.js
+++ json-parser.js
@@ -1,3 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+const example = fs
+  .readFileSync(path.join(__dirname, "./example.json"))
+  .toString();
+//console.log(example);
+
 const nullParser = input => {
   return input.startsWith("null") ? [null, input.slice(4)] : null;
 };
@@ -8,7 +15,35 @@ const booleanParser = input => {
 };
 
 const numberParser = input => {
-  let numberPattern = /^[+-]?\d*\.?(\d+[eE]?[+-]?)?\d+$/;
+  let numberPattern = /^[+-]?\d*\.?(\d+[eE]?[+-]?)?\d+/;
   let res = input.match(numberPattern);
   return res ? [parseFloat(res[0]), input.slice(res[0].length)] : null;
 };
+
+const stringParser = input => {
+  if (input.startsWith('"')) {
+    let slicedInput = input.slice(1);
+    let len = 0;
+    for (let i = 0; slicedInput.length; i++) {
+      console.log(slicedInput[i]);
+      if (slicedInput[i] === '"') {
+        if (slicedInput[i - 1] === "\\") {
+          len++;
+        } else {
+          len++;
+          break;
+        }
+      } else {
+        len++;
+      }
+    }
+    return [input.slice(0, len + 1), input.slice(len + 1)];
+  }
+  return null;
+};
+
+const spaceParser = input => {
+  input = input.replace(/\s+/, "");
+  return input;
+};
+console.log(spaceParser(example));
